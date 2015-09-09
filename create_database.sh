8e957e7ab29bdc09e48925b0057ab4bb8d9e62d2
ttop5 <ttop5@qq.com>
2015-09-10 00:57:41 +0800

    创建数据库和user表

---
+++ create_database.sh
@@ -0,0 +1,23 @@
+mysql -u'root' -p'passwd' <<EOF
+
+CREATE DATABASE student_manage;
+USE student_manage;
+
+CREATE TABLE user(
+user_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+school_num int(20) NOT NULL,
+password varchar(120) NOT NULL,
+name varchar(50) NOT NULL,
+sex varchar(10),
+grade varchar(100),
+major varchar(100),
+class varchar(100),
+qq varchar(20),
+phone varchar(20),
+email varchar(60),
+adress varchar(200),
+role int(2) NOT NULL DEFAULT '2' 
+)charset=utf8;
+
+INSERT INTO user VALUES ('', '13110581072', 'passwd', 'admin', '男', '2013', '计算机科学与技术', '计科1302', '1427154738', '18753377101', 'ttop5@qq.com', '云南省昆明市石林县', '0'); 
+EOF
