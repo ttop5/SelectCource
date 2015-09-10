ea00f9b210a3c84ee3ba4ba31e8da817aba27cd3
ttop5 <ttop5@qq.com>
2015-09-10 16:54:18 +0800

    增加school字段

--- create_database.sh
+++ create_database.sh
@@ -9,6 +9,7 @@ school_num int(20) NOT NULL,
 name varchar(50) NOT NULL,
 sex varchar(10),
 grade varchar(100),
+school varchar(100),
 major varchar(100),
 class varchar(100),
 qq varchar(20),
@@ -19,5 +20,5 @@ adress varchar(200),
 role int(2) NOT NULL DEFAULT '2' 
 )charset=utf8;
 
-INSERT INTO user VALUES ('', '13110581072', 'admin', '男', '2013', '计算机科学与技术', '计科1302', '1427154738', '18753377101', 'ttop5@qq.com', 'passwd', '云南省昆明市石林县', '0'); 
+INSERT INTO user VALUES ('', '13110581072', 'admin', '男', '2013', '计算机科学与技术学院', '计算机科学与技术', '计科1302', '1427154738', '18753377101', 'ttop5@qq.com', 'passwd', '云南省昆明市石林县', '0'); 
 EOF
