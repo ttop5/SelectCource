2763f7fdaf17dae01cae3272df2d6e99b4ded69d
ttop5 <ttop5@qq.com>
2015-09-18 14:54:41 +0800

    update

--- create_database.sh
+++ create_database.sh
@@ -5,8 +5,8 @@ CREATE DATABASE student_manage DEFAULT CHARACTER SET utf8 COLLATE utf8_general_c
 USE student_manage;
 
 CREATE TABLE user(
-id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-school_num int(20) NOT NULL,
+user_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+school_num int(20) NOT NULL UNIQUE,
 name varchar(50) NOT NULL,
 sex varchar(10),
 grade varchar(100),
@@ -23,11 +23,11 @@ role int(2) NOT NULL DEFAULT '2'
 
 INSERT INTO user VALUES ('', '1234567890', 'admin', '', '', '', '', '', '', '', 'admin@qq.com', 'passwd', '', '0'); 
 INSERT INTO user VALUES ('', '1234567', 'teacher1', '女', '', '机械工程学院', '', '', '', '', 'teacher1@qq.com', 'passwd', '', '1'); 
-INSERT INTO user VALUES ('', '1234567', 'teacher2', '男', '', '材料学院', '', '', '', '', 'teacher2@qq.com', 'passwd', '', '1'); 
+INSERT INTO user VALUES ('', '2234567', 'teacher2', '男', '', '材料学院', '', '', '', '', 'teacher2@qq.com', 'passwd', '', '1'); 
 INSERT INTO user VALUES ('', '13110581072', 'ttop5', '男', '2013', '计算机科学与技术学院', '计算机科学与技术', '计科1302', '1427154738', '18753377101', 'ttop5@qq.com', 'passwd', '云南省昆明市石林县', '2');
 
 CREATE TABLE notes(
-id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+notes_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 title varchar(50) NOT NULL,
 start_time date NOT NULL,
 stop_time date NOT NULL,
@@ -37,7 +37,7 @@ description varchar(1000) NOT NULL
 INSERT INTO notes VALUES ('', '关于全校大一同学的选课通知', '2015-09-16', '2015-09-17', '此次选课将持续一周的时间，共三次机会，请同学们按时限选课，完成应修的学分。注：大一的每学期最多选两门课！');
 
 CREATE TABLE classroom(
-id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+classroom_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 capacity  int(5) NOT NULL,
 location varchar(10) NOT NULL
 )CHARSET=utf8;
@@ -48,12 +48,12 @@ INSERT INTO classroom VALUES ('', '80', '4#101');
 INSERT INTO classroom VALUES ('', '160', '5#206');
 
 CREATE TABLE cource(
-id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-cource varchar(20) NOT NULL,
-teacher_id int(11) NOT NULL,
-classroom_id int(11) NOT NULL,
-CONSTRAINT cource_teacher FOREIGN KEY (teacher_id) REFERENCES user(id),
-CONSTRAINT cource_classroom FOREIGN KEY (classroom_id) REFERENCES classroom(id)
+cource_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+cource_name varchar(20) NOT NULL,
+teacher int(11) NOT NULL,
+classroom int(11) NOT NULL,
+CONSTRAINT cource_teacher FOREIGN KEY (teacher) REFERENCES user(user_id),
+CONSTRAINT cource_classroom FOREIGN KEY (classroom) REFERENCES classroom(classroom_id)
 )CHARSET=utf8;
 
 INSERT INTO cource VALUES('', '计算机网络', '2', '1');
@@ -62,25 +62,25 @@ INSERT INTO cource VALUES('', '西方艺术鉴赏', '2', '3');
 INSERT INTO cource VALUES('', '新能源技术', '3', '4');
 
 CREATE TABLE scource(
-id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-student_id int(11) NOT NULL,
+scource_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+student int(11) NOT NULL,
 cource varchar(20) NOT NULL,
-CONSTRAINT scource_student FOREIGN KEY (student_id) REFERENCES user(id)
+CONSTRAINT scource_student FOREIGN KEY (student) REFERENCES user(User_id)
 )CHARSET=utf8;
 
-INSERT INTO scource VALUES ('', '3', '1');
-INSERT INTO scource VALUES ('', '3', '2');
+INSERT INTO scource VALUES ('', '4', '1');
+INSERT INTO scource VALUES ('', '4', '2');
 
 CREATE TABLE score(
-id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-student_id int(11) NOT NULL, 
-cource_id int(11) NOT NULL, 
+score_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+student int(11) NOT NULL, 
+cource int(11) NOT NULL, 
 score varchar(5) NOT NULL,
-CONSTRAINT score_student FOREIGN KEY (student_id) REFERENCES user(id),
-CONSTRAINT score_cource FOREIGN KEY (cource_id) REFERENCES cource(id)
+CONSTRAINT score_student FOREIGN KEY (student) REFERENCES user(user_id),
+CONSTRAINT score_cource FOREIGN KEY (cource) REFERENCES cource(cource_id)
 )CHARSET=utf8;
 
-INSERT INTO score VALUES('', '3', '1', '95');
-INSERT INTO score VALUES('', '3', '2', '60');
+INSERT INTO score VALUES('', '4', '1', '95');
+INSERT INTO score VALUES('', '4', '2', '60');
 
 EOF
